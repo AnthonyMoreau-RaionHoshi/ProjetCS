34eeed7581057a93a0eb5ee7465094c95a3e6df8
anijiber <99720963+Anijiberschi@users.noreply.github.com>
2022-03-19 15:54:53 +0100

    Fonctions pour déplacement
    J'ai mis les déplacement sur 2 fonctions et crée 3 variables bool pour le déplacement. Possibilité de changer notre déplacement

--- Assets/Scrips/Players.cs
+++ Assets/Scrips/Players.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Players : MonoBehaviour
 {
-    public int Keys;
+    public int KeysObject;
     private Animation My_Animation;
-    bool My_IsPlaying_forward = false;
+    private bool My_IsPlaying_forward = false; // A mettre dans un tableau ?
+    private bool key_pressed_left = false;     // A mettre dans un tableau ?
+    private bool key_pressed_right = false;    // A mettre dans un tableau ?
+    private bool key_pressed_down = false;     // A mettre dans un tableau ?
+     // private KeyCode[] keyscode = new []{KeyCode.UpArrow, KeyCode.RightArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow }; Idée pour simplifer le code ?
     Transform AnimationPlayer;
     Rigidbody PlayerRigidBody;
 
@@ -24,39 +28,64 @@ public class Players : MonoBehaviour
         {   
             My_IsPlaying_forward = true;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) == true)
         {
-            AnimationPlayer.Translate(0, 0, (float)-1.25);
+            key_pressed_right = true;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) == true)
+        else if (Input.GetKeyDown(KeyCode.DownArrow) == true)
         {
-            AnimationPlayer.Translate((float)-1.25, 0, 0);
+            key_pressed_down = true;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
         {
-            AnimationPlayer.Translate(0, 0, (float)1.25);
+            key_pressed_left=true;
         }
     }
     private void FixedUpdate()
     {
-        if (My_IsPlaying_forward == true )
+        WhereIGo();
+        iFall();
+       
+    }
+
+
+    public void WhereIGo() // Fonction pour connaitre la direction du joueur.
+    {
+        if (My_IsPlaying_forward == true)
         {
             AnimationPlayer.Translate((float)1.25, 0, 0);
-            My_IsPlaying_forward = false ;
+            My_IsPlaying_forward = false;
         }
+        else if (key_pressed_down == true) {
+            AnimationPlayer.Translate((float)-1.25, 0, 0);
+            key_pressed_down = false ;
+        }
+        else if (key_pressed_right == true) {
+            AnimationPlayer.Translate(0, 0, (float)-1.25);
+            key_pressed_right = false ;
+        }
+        else if (key_pressed_left == true) {
+            AnimationPlayer.Translate(0,0, (float)1.25);
+            key_pressed_left = false ;
+        }
+    }
+
+    public void iFall() // Vérifie si il tombe 
+    {
         if (transform.position.y < 1.20)
         {
             PlayerRigidBody.transform.position = -PositionInit;
-            
+
         }
     }
+
 
     public void OnTriggerEnter(Collider Col)
     {
         if (Col.gameObject.tag == "Keys") 
         {
             Debug.Log("Clé collecté");
-            Keys++;
+            KeysObject++;
             Destroy(Col.gameObject);
         }
     }

366bca7172905fd31be66365aab81cb523387ffb
anijiber <99720963+Anijiberschi@users.noreply.github.com>
2022-03-28 00:01:17 +0200

    Pitié

--- Assets/Scrips/Players.cs
+++ Assets/Scrips/Players.cs
@@ -1,86 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Players : MonoBehaviour
 {
-    public int KeysObject;
+
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private int KeysObject;
+    TextMesh status;
     private Animation My_Animation;
-    private bool My_IsPlaying_forward = false; // A mettre dans un tableau ?
-    private bool key_pressed_left = false;     // A mettre dans un tableau ?
-    private bool key_pressed_right = false;    // A mettre dans un tableau ?
-    private bool key_pressed_down = false;     // A mettre dans un tableau ?
-     // private KeyCode[] keyscode = new []{KeyCode.UpArrow, KeyCode.RightArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow }; Idée pour simplifer le code ?
+    private bool[] boolKeys = new bool[] { false /*Left*/, false/*Arrow*/, false/*Down*/, false /*Right*/};
+    private KeyCode[] keyscode = new []{ KeyCode.LeftArrow,KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightArrow};
     Transform AnimationPlayer;
     Rigidbody PlayerRigidBody;
+    private Vector3 PositionInit = new Vector3((float)-0.126000002, (float)1.60000002, 0);
 
-    Vector3 PositionInit =new Vector3(0,-2,0);
     void Start()
     {
+
+        status = GameObject.Find("Status").GetComponent<TextMesh>();
         AnimationPlayer = GetComponentInChildren<Transform>();
         My_Animation = GetComponentInChildren<Animation>();
         PlayerRigidBody = GetComponent<Rigidbody>();
+
     }
 
     void Update()
-    {   
-        if (Input.GetKeyDown(KeyCode.UpArrow) == true)
-        {   
-            My_IsPlaying_forward = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+    {
+
+
+
+        status.text = "Clés : " + KeysObject;
+
+        for (int i = 0; i < keyscode.Length; i++) // Check touts les inputs. 
         {
-            key_pressed_right = true;
+            if (Input.GetKeyDown(keyscode[i]))
+            {
+                boolKeys[i] = true;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) == true)
-        {
-            key_pressed_down = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
-        {
-            key_pressed_left=true;
-        }
+
+
+        
     }
     private void FixedUpdate()
     {
+        Debug.Log(Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length); //Permetde vérifier les points de contacts avec le player.
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 0)
+        {
+            iFall();
+            return;
+        }
+
         WhereIGo();
-        iFall();
-       
+
     }
 
 
     public void WhereIGo() // Fonction pour connaitre la direction du joueur.
     {
-        if (My_IsPlaying_forward == true)
+
+
+
+        if (boolKeys[0])
         {
+            AnimationPlayer.Translate(0, 0, (float)1.25);
+            boolKeys[0] = false;
+            
+        }
+        else if (boolKeys[1]) {
+
             AnimationPlayer.Translate((float)1.25, 0, 0);
-            My_IsPlaying_forward = false;
+            
+            boolKeys[1] = false ;
         }
-        else if (key_pressed_down == true) {
+        else if (boolKeys[2]) {
             AnimationPlayer.Translate((float)-1.25, 0, 0);
-            key_pressed_down = false ;
+            boolKeys[2] = false ;
         }
-        else if (key_pressed_right == true) {
+        else if (boolKeys[3]) {
             AnimationPlayer.Translate(0, 0, (float)-1.25);
-            key_pressed_right = false ;
-        }
-        else if (key_pressed_left == true) {
-            AnimationPlayer.Translate(0,0, (float)1.25);
-            key_pressed_left = false ;
+            
+            
+            boolKeys[3] = false ;
         }
     }
 
-    public void iFall() // Vérifie si il tombe 
+    public void iFall() // Vérifie si il tombe sous un point y.
     {
-        if (transform.position.y < 1.20)
+        if (transform.position.y < 0)
         {
-            PlayerRigidBody.transform.position = -PositionInit;
-
+            PlayerRigidBody.transform.position = PositionInit;
         }
     }
 
 
-    public void OnTriggerEnter(Collider Col)
+
+    public void OnTriggerEnter(Collider Col) // Check la colisition avec un objet key.
     {
         if (Col.gameObject.tag == "Keys") 
         {

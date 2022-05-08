de7a90bed33ec4ec2d9a3fd5b1c74b9a23857759
Imadghali <100768879+Imadghali@users.noreply.github.com>
2022-05-08 13:29:00 +0200

    charger/sauvegarder implementé

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class menu : MonoBehaviour
 {
+    private static string SaveSeparator = "%VALUE%";
     // le bouton quitter ne peu etre testé que sur un .exe vu qu il quitte l application (pas de stress si il ne fonctionne pas sur unity).
     public static void Jouer() 
     {
@@ -13,9 +15,25 @@ public class menu : MonoBehaviour
         Players.Keys = 0;
         DataPlayer.LevelEnCours = 0;
     }
-    public static void Charger()
+    public static void save()
     {
+        string[] content = new string[]
+        {
+            DataPlayer.LevelEnCours.ToString(),
+            Players.Lives.ToString(),
+        };
+        string SaveString = string.Join(SaveSeparator, content);
+        File.WriteAllText(Application.dataPath + "/data.txt", SaveString);
+    }
+
+    public static void load()
+    {
+        string SaveString = File.ReadAllText(Application.dataPath + "/data.txt");
+        string[] content = SaveString.Split(new[] { SaveSeparator }, System.StringSplitOptions.None);
+        DataPlayer.LevelEnCours = int.Parse(content[0]);
+        Players.Lives = int.Parse(content[1]);
         SceneManager.LoadScene(DataPlayer.SceneActif[DataPlayer.LevelEnCours]);
+
     }
     public static void Quitter()
     {
